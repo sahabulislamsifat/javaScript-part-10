076bc01d47a989a980b655f8bf914916ed4ec57c
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-29 15:36:39 +0600

    explore dom method

---
+++ document_object_model.js
@@ -0,0 +1,6 @@
+// document.getElementById("demo");
+// console.log(document.getElementById("demo"));
+// console.dir(document.getElementById("demo"))
+const p = document.getElementById("demo");
+
+p.innerHTML = "Test Zero";
