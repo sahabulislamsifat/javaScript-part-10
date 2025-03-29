2aebbf8589806f1f598301700fc919165a91af75
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-30 02:56:55 +0600

    DOM project by LWS

--- document_object_model.js
+++
@@ -1,6 +0,0 @@
-// document.getElementById("demo");
-// console.log(document.getElementById("demo"));
-// console.dir(document.getElementById("demo"))
-const p = document.getElementById("demo");
-
-p.innerHTML = "Test Zero";
